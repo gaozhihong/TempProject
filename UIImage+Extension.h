a4baa488c09e92936a2bbb534f53171ef5ecf461
gaozhihong <18710296168@163.com>
2018-01-09 20:51:47 +0800

    first

---
+++ UIImage+Extension.h
@@ -0,0 +1,19 @@
+//
+//  UIImage+Extension.h
+//  冒泡排序
+//
+//  Created by gaozhihong on 2017/8/24.
+//  Copyright © 2017年 gaozhihong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (Extension)
+
+ // 切圆
+-(UIImage*)imageClip;
+ // 切圆角
+-(UIImage*)imageClipWithConner:(float)radius;
+
+
+@end
